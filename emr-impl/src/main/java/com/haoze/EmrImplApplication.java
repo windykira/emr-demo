50e96e947867f62763d823789225e1b9f01947c8
windy <windylucky@yeah.net>
2018-07-19 22:10:11 +0800

    emr

---
+++ emr-impl/src/main/java/com/haoze/EmrImplApplication.java
@@ -0,0 +1,12 @@
+package com.haoze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmrImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmrImplApplication.class, args);
+	}
+}
---
+++ emr-model/src/main/java/com/haoze/EmrModelApplication.java
@@ -0,0 +1,12 @@
+package com.haoze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmrModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmrModelApplication.class, args);
+	}
+}
